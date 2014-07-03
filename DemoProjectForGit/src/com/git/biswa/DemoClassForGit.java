2f9e4327b9e85cef789fdfcb5624647e9b2c7b4d
BRC1988 <brc1988@gmail.com>
2014-07-03 22:54:58 +0530

    1st commit

---
+++ DemoProjectForGit/src/com/git/biswa/DemoClassForGit.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.git.biswa;
+
+/**
+ * @author Biswa
+ *
+ */
+public class DemoClassForGit {
+
+	/**
+	 * 
+	 */
+	public DemoClassForGit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Git");
+
+	}
+
+}
